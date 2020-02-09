ce026745f161a10941b8e8e3f53583c1a201608d
Mark Stenglein <mark@stengle.in>
2020-02-08 22:32:05 -0500

    MODELS: Documentation and normalization
    This commit:
      - Documents all of the model classes
      - Normalizes all of the attribute names to attr_name
        instead of using attrName
    
    Signed-off-by: Mark Stenglein <mark@stengle.in>

--- paperlesspermission/models.py
+++ paperlesspermission/models.py
@@ -1,10 +1,25 @@
+"""Defines all of the data models used in the application.
+
+This file is used to define all of the data models used by Paperless Permission.
+"""
+
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 class Person(models.Model):
-    personID = models.IntegerField(unique=True)
-    firstName = models.CharField(max_length=200)
-    lastName = models.CharField(max_length=200)
+    """This is an abstract class that defines the common attributes of people.
+
+    Attributes:
+        person_id (CharField): The unique identifier for the person
+        first_name (CharField): First Name
+        last_name (CharField): Last Name
+        email (CharField): Email address
+        cell_number (PhoneNumberField): Cell phone number
+        notify_cell (BooleanField): Whether or not to send SMS messages
+    """
+    person_id = models.IntegerField(unique=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
     cell_number = PhoneNumberField()
     notify_cell = models.BooleanField()
@@ -13,45 +28,100 @@ class Person(models.Model):
         abstract = True
 
     def __str__(self):
-        return ("%s %s" % self.firstName, self.lastName)
+        return ("%s %s" % self.first_name, self.last_name)
 
 class Guardian(Person):
+    """Defines a student's parent/guardian.
+
+    Extends the `Person` class. In practice these end up being Mothers, Fathers,
+    Grandparents, or other legal custodians.
+
+    Attributes:
+        relationship (CharField): The relationship of the Guardian to the Student
+        students (ManyToManyField): All of the students the guardian is related to
+    """
     relationship = models.CharField(max_length=30)
     students = models.ManyToManyField('Student')
 
 class Student(Person):
-    gradeLevel = models.CharField(max_length=30)
+    """Defines a Student.
+
+    Extends the `Person` class.
+
+    Attributes:
+        grade_level (CharField): The grade level of the Student
+        guardians (ManyToManyField): All of the `Guardians` related to the Student
+    """
+    grade_level = models.CharField(max_length=30)
     guardians = models.ManyToManyField(Guardian)
 
 class Faculty(Person):
-    preferredName = models.CharField(max_length=200)
+    """Defines Faculty/Staff that will run field trips.
+
+    Attributes:
+        preferred_name (CharField): Teachers might prefer to go by Mrs/Mr <last_name>
+    """
+    preferred_name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.preferredName
+        return self.preferred_name
 
 class Course(models.Model):
-    courseNumber = models.CharField(unique=True, max_length=30)
-    courseName = models.CharField(max_length=200)
+    """Defines a `Course`.
+
+    This is the container for individual `Section` objects.
+
+    Attributes:
+        course_number (CharField): The course identifier
+        course_name (CharField): The name of the course
+    """
+    course_number = models.CharField(unique=True, max_length=30)
+    course_name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.courseName
+        return self.course_name
 
 class Section(models.Model):
+    """Defines a `Section` of a `Course`.
+
+    Attributes:
+        course (ForeignKey): FK to `Course`
+        section_number (CharField): The section identifier
+        teacher (ForeignKey): FK to `Faculty`
+        school_year (CharField): School year the section is held in
+        room (CharField): Room the section is held in
+        period (CharField): Period the section is held during
+    """
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    sectionNumber = models.CharField(unique=False, max_length=30)
+    section_number = models.CharField(unique=False, max_length=30)
     teacher = models.ForeignKey(Faculty, on_delete=models.CASCADE)
-    courseName = models.CharField(max_length=200)
-    schoolYear = models.CharField(max_length=30)
+    school_year = models.CharField(max_length=30)
     room = models.CharField(max_length=30)
     period = models.CharField(max_length=30)
 
     def __str__(self):
-        return ("%s - Section %s" % self.course, self.sectionNumber)
+        return ("%s - Section %s" % self.course, self.section_number)
 
 class FieldTrip(models.Model):
+    """Defines a `FieldTrip`.
+
+    Attributes:
+        name (CharField): Name of the Field Trip
+        start_date (DateTimeField): Start date/time.
+        end_date (DateTimeField): End date/time.
+        students (ManyToManyField): All students invited to attend the Field
+            Trip
+        faculty (ManyToManyField): All faculty/staff invited to administer the
+            Field Trip
+        courses (ManyToManyField): Students enrolled in these courses during the
+            relevent school year/semester will automatically be invited to
+            attend this field trip
+        sections (ManyToManyField): Students enrolled in these sections will
+            automatically be invited to attend this field trip
+    """
     name = models.CharField(max_length=200)
-    fromDate = models.DateTimeField()
-    toDate = models.DateTimeField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
     students = models.ManyToManyField(Student)
     faculty = models.ManyToManyField(Faculty)
     courses = models.ManyToManyField(Course)
@@ -59,5 +129,3 @@ class FieldTrip(models.Model):
 
     def __str__(self):
         return self.name
-
-
