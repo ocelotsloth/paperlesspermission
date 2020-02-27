cfe78fd3ea14147cd405ff0985d9889d3161acdf
Mark Stenglein <mark@stengle.in>
2020-02-26 22:54:22 -0500

    PROJECT: DOCS: Adds auto docs page to /admin/doc
    This commit enables the automatic docs generator.
    
    Simply generate an admin user and navigate to /admin/doc
    to view the automatic documentation generated by django.
    
    Signed-off-by: Mark Stenglein <mark@stengle.in>

--- paperlesspermission/settings.py
+++ paperlesspermission/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admindocs',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.admindocs.middleware.XViewMiddleware',
 ]
 
 ROOT_URLCONF = 'paperlesspermission.urls'
--- paperlesspermission/urls.py
+++ paperlesspermission/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
 ]
